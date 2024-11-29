a05e4013dafe82c34a8dd2e7884e6dba0b761d16
Ahmed Ghlimi <aghlimi@e2r9p9.1337.ma>
2024-11-29 17:46:09 +0100

    add parsing to split

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,16 @@
 /*   By: aghlimi <aghlimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:28:54 by aghlimi           #+#    #+#             */
-/*   Updated: 2024/10/27 13:32:24 by aghlimi          ###   ########.fr       */
+/*   Updated: 2024/11/29 17:42:39 by aghlimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t countword(const char *s, char c)
+static size_t	countword(const char *s, char c)
 {
-	size_t number;
-	int index;
+	size_t	number;
+	int		index;
 
 	index = 0;
 	number = 0;
@@ -31,9 +31,9 @@ static size_t countword(const char *s, char c)
 	return (number);
 }
 
-static void freesplit(char **list, size_t size)
+static void	freesplit(char **list, size_t size)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i < size)
@@ -44,30 +44,47 @@ static void freesplit(char **list, size_t size)
 	}
 	free(list);
 }
-int skip(const char *s, int i, char c)
+int	where(const char *text, char c)
+{
+	int	i;
+
+	i = 0;
+	while (text && text[i])
+	{
+		if (text[i] == c)
+		{
+			return (i);
+		}
+		i++;
+	}
+	exit(1);
+	return (6);
+}
+int	skip(const char *s, int i, char c)
 {
 	if (s[i] == '\'' || s[i] == '"' || s[i] == '`')
-		return ft_strchr(s + i + 1, s[i]) - s + i;
+		return (where(s + i + 1, s[i]) + 2 + i);
 	while (s[i] != c && s[i])
 		i++;
-	return i;
+	return (i);
 }
 
-void parse(char *text)
+void	parse(char *text)
 {
 	if (!text)
 		return ;
-	if (ft_strchr("\"'`", text[0]) && ft_strchr("\"'`", text[ft_strlen(text) - 1]))
+	if (ft_strchr("\"'`", text[0]) && ft_strchr("\"'`", text[ft_strlen(text)
+			- 1]))
 	{
 		text[ft_strlen(text) - 1] = 0;
 		ft_memmove(text, text + 1, ft_strlen(text));
 	}
 }
-static char **splitloop(const char *s, char c, char **list)
+static char	**splitloop(const char *s, char c, char **list)
 {
-	int i;
-	int index;
-	size_t size;
+	int		i;
+	int		index;
+	size_t	size;
 
 	size = 0;
 	i = 0;
@@ -89,9 +106,9 @@ static char **splitloop(const char *s, char c, char **list)
 	return (list);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	char **list;
+	char	**list;
 
 	if (!s)
 		return (NULL);
@@ -101,17 +118,3 @@ char **ft_split(char const *s, char c)
 	list = splitloop(s, c, list);
 	return (list);
 }
-int main(int argc, char const *argv[])
-{
-	if (argc - 1)
-	{
-		int i = 0;
-		char **list = ft_split(argv[1], ' ');
-		while (list[i])
-		{
-			printf("%s\n", list[i++]);
-		}
-	}
-	return 0;
-}
-// awk '{printf $1}'
